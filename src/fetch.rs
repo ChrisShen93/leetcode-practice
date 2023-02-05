61f7bb0a5d1f1a62c576b9833e9b2088ba3dd100
ChrisShen93 <chrisshen93@gmail.com>
2023-02-05 21:24:03 +0800

    refactor: use one cargo workspace

---
+++ src/fetch.rs
@@ -0,0 +1,9 @@
+// use std::collections::HashMap;
+
+const PROBLEMS_URL: &str = "https://leetcode.com/api/problems/algorithms/";
+
+pub async fn get_problems() -> Result<(), Box<dyn std::error::Error>> {
+	let resp = reqwest::get(PROBLEMS_URL).await;
+	println!("this is test {:#?}", resp);
+	Ok(())
+}
---
+++ src/solutions/s0001_two_sum.rs
@@ -0,0 +1,61 @@
+/**
+ * [1] Two Sum
+ *
+ * Given an array of integers, return indices of the two numbers such that they
+ * add up to a specific target.
+ *
+ * You may assume that each input would have exactly one solution, and you may
+ * not use the same element twice.
+ *
+ * Example:
+ *
+ *
+ * Given nums = [2, 7, 11, 15], target = 9,
+ *
+ * Because nums[0] + nums[1] = 2 + 7 = 9,
+ * return [0, 1].
+ *
+ */
+use std::collections::HashMap;
+
+pub struct Solution {}
+
+impl Solution {
+    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        // this will work
+        // let mut hash = HashMap::new();
+        // for (index, item) in nums.iter().enumerate() {
+        //     if hash.contains_key(item) {
+        //         return vec![hash[item], index as i32];
+        //     } else {
+        //         hash.insert(target - item, index as i32);
+        //     }
+        // }
+        // unreachable!();
+
+        // better way
+        let mut map = HashMap::with_capacity(nums.len());
+        for (index, num) in nums.iter().enumerate() {
+            match map.get(&(target - num)) {
+                None => {
+                    map.insert(num, index);
+                }
+                Some(sub_index) => return vec![*sub_index as i32, index as i32],
+            }
+        }
+        vec![]
+    }
+}
+
+// submission codes end
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test() {
+        assert_eq!(vec![0, 1], Solution::two_sum(vec![2, 7, 11, 15], 9));
+        assert_eq!(vec![1, 2], Solution::two_sum(vec![3, 2, 4], 6));
+    }
+}
