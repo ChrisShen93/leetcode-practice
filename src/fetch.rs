77311facb8e7340224ff11e0c1db554ed5c89cb8
沈渊海 <shenyuanhai@bytedance.com>
2024-03-02 23:41:41 +0800

    prepare to fetch questions

--- src/fetch.rs
+++ src/fetch.rs
@@ -3,7 +3,10 @@
 const PROBLEMS_URL: &str = "https://leetcode.com/api/problems/algorithms/";
 
 pub async fn get_problems() -> Result<(), Box<dyn std::error::Error>> {
-	let resp = reqwest::get(PROBLEMS_URL).await;
-	println!("this is test {:#?}", resp);
-	Ok(())
+    let resp = reqwest::get(PROBLEMS_URL).await;
+    println!("this is test {:#?}", resp);
+    Ok(())
 }
+
+#[cfg(test)]
+mod tests {}
